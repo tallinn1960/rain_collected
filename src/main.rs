d8570823d794e78711e2a4361b6389b734215148
Olaf Schlüter <olaf.schlueter@secunet.com>
2024-03-04 17:31:46 +0100

    Moved timing measurement into main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 //! rain_collected
+//! 
+//! This program calculates the amount of water that can be collected in a terrain.
+//! The terrain is represented as a vector of elevations. The elevation at each index of the vector
+//! represents the height of the terrain at that point.
+//! 
+const N: usize = 10000;
+
 fn main() {
-    let terrain = [1, 4, 2, 5, 3, 6, 4, 7];
-    let rain_collected = compute_rain_collected(&terrain);
-    println!("Rain collected: {}", rain_collected);
-    let rain_collected_parallel = compute_rain_collected_parallel(&terrain);
-    println!("Rain collected parallel: {}", rain_collected_parallel);
+    use std::time::Instant;
+    // generate a large terrain with N randome elevations between 0 and 100
+    let terrain: Vec<i64> = (0..N).map(|_| rand::random::<i64>() % 100).collect();
+
+    let start_time = Instant::now();
+    compute_rain_collected(&terrain);
+    let end_time = Instant::now();
+    let execution_time = end_time.duration_since(start_time).as_micros();
+
+    println!(
+        "Execution time compute_rain_collected: {} microseconds",
+        execution_time
+    );
+
+    let start_time = Instant::now();
+    compute_rain_collected_parallel(&terrain);
+    let end_time = Instant::now();
+    let execution_time = end_time.duration_since(start_time).as_micros();
+
+    println!(
+        "Execution time compute_rain_collected_parallel: {} microseconds",
+        execution_time
+    );
 }
 
 /// Compute the capacity of water that can be collected in the given terrain.
@@ -139,32 +164,5 @@ mod tests {
         assert_eq!(compute_rain_collected(&terrain8), 5);
     }
 
-    const N: usize = 10000;
 
-    #[test]
-    fn calculate_execution_time() {
-        use std::time::Instant;
-        // generate a large terrain with N randome elevations between 0 and 100
-        let terrain: Vec<i64> = (0..N).map(|_| rand::random::<i64>() % 100).collect();
-
-        let start_time = Instant::now();
-        compute_rain_collected(&terrain);
-        let end_time = Instant::now();
-        let execution_time = end_time.duration_since(start_time).as_micros();
-
-        println!(
-            "Execution time compute_rain_collected: {} microseconds",
-            execution_time
-        );
-
-        let start_time = Instant::now();
-        compute_rain_collected_parallel(&terrain);
-        let end_time = Instant::now();
-        let execution_time = end_time.duration_since(start_time).as_micros();
-
-        println!(
-            "Execution time compute_rain_collected_parallel: {} microseconds",
-            execution_time
-        );
-    }
 }
