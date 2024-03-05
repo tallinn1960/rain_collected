796d69dce9467a8efb1c6b056019b254c716889e
Olaf Schlüter <olaf.schlueter@secunet.com>
2024-03-05 21:24:56 +0100

    Improved double pointer solution

--- src/main.rs
+++ src/main.rs
@@ -5,72 +5,72 @@ const N: i64 = 100000;
 fn main() {
     use std::time::Instant;
 
-    let terrain: Vec<i64> = (0..N).map(|_| rand::random::<i64>() % N).collect();
+    let terrain: Vec<i64> = (0..N).map(|_| (rand::random::<i64>() % N)).collect();
 
     let start_time = Instant::now();
-    let t = trap(&terrain);
+    let t1 = trap(&terrain);
     let end_time = Instant::now();
     let execution_time = end_time.duration_since(start_time).as_micros();
 
     println!("Execution time trap: {} microseconds", execution_time);
-    println!("Water capacity: {}", t);
+    println!("Water capacity: {}", t1);
 
     let start_time = Instant::now();
-    let t = trap2(&terrain);
+    let t2 = trap2(&terrain);
     let end_time = Instant::now();
     let execution_time = end_time.duration_since(start_time).as_micros();
 
     println!("Execution time trap2: {} microseconds", execution_time);
-    println!("Water capacity: {}", t);
-}
+    println!("Water capacity: {}", t2);
 
-fn trap(terrain: &[i64]) -> u64 {
-    if terrain.len() < 3 {
-        return 0;
+    if t1 != t2 {
+        println!("Results are different!");
+        println!("t1: {}", t1);
+        println!("t2: {}", t2);
+        println!("Terrain: ");
+        println!("{:?}", terrain);
     }
-
-    let index_maximum = terrain
-        .iter()
-        .enumerate()
-        .max_by_key(|&(_, v)| v)
-        .unwrap()
-        .0;
-
-    let (terrain_left_of_max_elevation, terrain_right_of_max_elevation) =
-        terrain.split_at(index_maximum);
-
-    let water_capacity_left = terrain_left_of_max_elevation
-        .iter()
-        .fold((i64::MIN, 0u64), |acc, &x| {
-            let stepsize = x.max(acc.0);
-            (stepsize, acc.1 + (stepsize - x) as u64)
-        })
-        .1;
-
-    let water_capacity_right = terrain_right_of_max_elevation
-        .iter()
-        .rfold((i64::MIN, 0u64), |acc, &x| {
-            let stepsize = x.max(acc.0);
-            (stepsize, acc.1 + (stepsize - x) as u64)
-        })
-        .1;
-
-    water_capacity_left + water_capacity_right
 }
 
+fn trap(height: &[i64]) -> u64 {
+    let mut hiter = height.into_iter();
+    std::iter::repeat(())
+        .scan(
+            (hiter.next().copied(), hiter.next_back().copied()),
+            |state, _| {
+                if let (Some(left), Some(right)) = *state {
+                    if left <= right {
+                        *state = (hiter.next().copied(), Some(right));
+                        Some(left)
+                    } else {
+                        *state = (Some(left), hiter.next_back().copied());
+                        Some(right)
+                    }
+                } else {
+                    None
+                }
+            },
+        )
+        .fold((i64::MIN, 0u64), |acc, x| {
+            let stepsize = x.max(acc.0);
+            (stepsize, acc.1 + (stepsize - x) as u64)
+        })
+        .1
+}
+
+// fastest solution from leetcode
 fn trap2(height: &[i64]) -> u64 {
     let (mut left, mut right) = (0, height.len() - 1);
-    let mut pool_height = 0i64;
+    let mut pool_height = i64::MIN;
     let mut trapped = 0u64;
 
     while left < right {
+        pool_height = pool_height.max(height[left].min(height[right]));
         if height[left] <= height[right] {
-            pool_height = pool_height.max(height[left]);
-            trapped += (pool_height - height[left]) as u64;
+            trapped += 0.max(pool_height - height[left]) as u64;
             left += 1;
         } else {
-            pool_height = pool_height.max(height[right]);
-            trapped += (pool_height - height[right]) as u64;
+            trapped += 0.max(pool_height - height[right]) as u64;
             right -= 1;
         }
     }
@@ -107,6 +107,15 @@ mod tests {
 
         let terrain8 = [5, 4, 2, 6, 6, 6, 4, 5];
         assert_eq!(trap(&terrain8), 5);
+
+        let terrain9 = [0, 1, -1, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+        assert_eq!(trap2(&terrain9), 7);
+
+        let terrain10 = [4, 2, 0, 3, 2, 5];
+        assert_eq!(trap2(&terrain10), 9);
+
+        let terrain11 = [0, -6, 0, -2, 8, -9, 0, 8, 9, -5];
+        assert_eq!(trap2(&terrain11), 33);
     }
 
     #[test]
@@ -134,5 +143,14 @@ mod tests {
 
         let terrain8 = [5, 4, 2, 6, 6, 6, 4, 5];
         assert_eq!(trap2(&terrain8), 5);
+
+        let terrain9 = [0, 1, -1, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+        assert_eq!(trap2(&terrain9), 7);
+
+        let terrain10 = [4, 2, 0, 3, 2, 5];
+        assert_eq!(trap2(&terrain10), 9);
+
+        let terrain11 = [0, -6, 0, -2, 8, -9, 0, 8, 9, -5];
+        assert_eq!(trap2(&terrain11), 33);
     }
 }
