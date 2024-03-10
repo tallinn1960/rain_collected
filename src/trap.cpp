f2b16530d46b62ed564d9dd3f3b57e2ea796c9ed
Olaf Schlüter <tallinn1960@yahoo.de>
2024-03-10 01:26:47 +0100

    removed unused headers

--- src/trap.cpp
+++ src/trap.cpp
@@ -1,40 +1,38 @@
 #include <vector>
 #include <iterator>
-#include <random>
 #include <span>
 #include <algorithm>
 #include <numeric>
 #include <limits>
-#include <iostream>
 
 using namespace std;
 
 unsigned long trap_cpp(std::span<long> v) {
-        if (v.size() < 3)
-        {
-            return 0;
-        }
+    if (v.size() < 3)
+    {
+        return 0;
+    }
 
-        vector u(v.size(), 0);
+    vector u(v.size(), 0);
 
-        auto it = max_element(begin(v), end(v));
+    auto it = max_element(begin(v), end(v));
 
-        inclusive_scan(begin(v), next(it), begin(u),
-                       [](auto a, auto b)
-        {
-            return max(a, b);
-        });
+    inclusive_scan(begin(v), next(it), begin(u),
+                   [](auto a, auto b)
+    {
+        return max(a, b);
+    });
 
-        inclusive_scan(rbegin(v), reverse_iterator(it),
-                       rbegin(u),
-                       [](auto a, auto b)
-        {
-            return max(a, b);
-        });
+    inclusive_scan(rbegin(v), reverse_iterator(it),
+                   rbegin(u),
+                   [](auto a, auto b)
+    {
+        return max(a, b);
+    });
 
-        return transform_reduce(cbegin(u), cend(u), cbegin(v), 0,
-                                std::plus<>(),
-                                std::minus<>());
+    return transform_reduce(cbegin(u), cend(u), cbegin(v), 0,
+                            std::plus<>(),
+                            std::minus<>());
 }
 
 extern "C" {
@@ -46,20 +44,20 @@ extern "C" {
 
 
 unsigned long trap_cpp_dp(std::span<long> height) {
-            size_t l = 0, r = height.size()-1;
-        long level = std::numeric_limits<long>::min();
-        unsigned long  water = 0;
-        while (l < r) {
-            level = max(level, min(height[l], height[r]));
-            if (height[l] < height[r]) {
-                water += level - height[l];
-                l += 1;
-            } else {
-                water += level - height[r];
-                r -= 1;
-            }
+    size_t l = 0, r = height.size()-1;
+    long level = std::numeric_limits<long>::min();
+    unsigned long  water = 0;
+    while (l < r) {
+        level = max(level, min(height[l], height[r]));
+        if (height[l] < height[r]) {
+            water += level - height[l];
+            l += 1;
+        } else {
+            water += level - height[r];
+            r -= 1;
         }
-        return water;
+    }
+    return water;
 }
 
 extern "C" {
