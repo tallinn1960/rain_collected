a30830aafea826deb2468f9142deeab9777fbc78
Olaf Schlüter <tallinn1960@yahoo.de>
2024-03-25 19:42:06 +0100

    shows linking problem when using cargo bench

--- trap_cpp/build.rs
+++ trap_cpp/build.rs
@@ -4,8 +4,7 @@ use cmake::Config;
 fn main() {
     let dst = Config::new(".")
         .define("CMAKE_EXPORT_COMPILE_COMMANDS", "YES")
-        .generator("Ninja Multi-Config")
         .build();
     println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=dylib=trap_cpp");
+    println!("cargo:rustc-link-lib=static=trap_cpp");
 }
