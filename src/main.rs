9d81459daa3e49dd633a7a56534ad77afb1ea103
Olaf Schlüter <tallinn1960@yahoo.de>
2024-06-27 10:28:24 +0200

    Made swift a feature

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 use trap_cpp::trap_cpp_dp;
 use trap_rust::compute_rain_collected;
+#[cfg(feature = "swift")]
 use trap_swift::trap_swift;
 use trap_zig::trap_zig_ffi;
 
@@ -13,8 +14,7 @@ const N: i64 = 100000;
 fn main() {
     use std::time::Instant;
 
-    let terrain: Vec<i64> =
-        (0..N).map(|_| rand::random::<i64>() % N).collect();
+    let terrain: Vec<i64> = (0..N).map(|_| rand::random::<i64>() % N).collect();
 
     let start_time = Instant::now();
     let t1 = compute_rain_collected(&terrain);
@@ -23,12 +23,15 @@ fn main() {
     println!("Execution time trap: {} microseconds", execution_time);
     println!("Water capacity: {}", t1);
 
-    let start_time = Instant::now();
-    let t1 = trap_swift(&terrain);
-    let execution_time = start_time.elapsed().as_micros();
+    #[cfg(feature = "swift")]
+    {
+        let start_time = Instant::now();
+        let t1 = trap_swift(&terrain);
+        let execution_time = start_time.elapsed().as_micros();
 
-    println!("Execution time trap_swift: {} microseconds", execution_time);
-    println!("Water capacity: {}", t1);
+        println!("Execution time trap_swift: {} microseconds", execution_time);
+        println!("Water capacity: {}", t1);
+    }
 
     let start_time = Instant::now();
     let t1 = trap_cpp_dp(&terrain);
@@ -49,5 +52,4 @@ fn main() {
         execution_time
     );
     println!("Water capacity: {}", t1);
-    
 }
